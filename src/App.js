05061f864df8d776e694e9dded059996a97a4eed
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2021-09-15 19:34:46 -0400

    Forms finished

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Get in touch</h1>
+      <h1>Contact</h1>
       <Form />
+      <p>The HTML version of the Form can be found <a href="https://github.com/MelvinPeepers/netlify_standard_form_react/blob/main/public/index.html">here</a></p>
+      <p>The JSX version of the Form can be found <a href="https://github.com/MelvinPeepers/netlify_standard_form_react/blob/main/src/component/Form.js">here</a></p>
+      <p>For React you will need both the HTML and JSX version of the form for Netlify.</p>
     </div>
   );
 }
